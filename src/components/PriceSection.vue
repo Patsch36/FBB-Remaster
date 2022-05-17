455f5fb2a6a125b5169187d916fd0eda2c9a220c
Patrick Scheich <scheichpatrick@gmail.com>
2022-05-17 16:43:56 +0200

    Template OfferCard

---
+++ src/components/PriceSection.vue
@@ -0,0 +1,85 @@
+<template>
+    <section style="
+      height: auto;
+      min-height: 90vh;
+      width: 100vw;">
+        <v-card>
+            <v-container>
+                <v-row class="mx-auto">
+                    <v-col cols="12" md="4">
+                        <v-card color="transparent" height="80vh" style="border: 1px solid rgb(var(--v-theme-textcolor))">
+                            <v-card-title class="text-h5 header">
+                                Unlimited music now
+                            </v-card-title>
+
+                            <div class="text-container pa-6" style="height: 75%">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias similique rem, est impedit laboriosam amet doloremque commodi aliquam blanditiis! Vel maiores officiis non impedit ipsum quos incidunt consequuntur repellat quaerat?
+                            </div>
+
+                            <v-card-actions class="action-container">
+                                <v-btn depressed variant="outlined" color="primary">
+                                    Angebot einholen
+                                </v-btn>
+                            </v-card-actions>
+                        </v-card>
+                    </v-col>
+
+                    <v-col cols="12" md="4">
+                        <v-card color="transparent" height="80vh" style="border: 1px solid rgb(var(--v-theme-textcolor))">
+                            <v-card-title class="text-h5 header">
+                                Unlimited music now
+                            </v-card-title>
+
+                            <div class="text-container pa-6" style="height: 75%">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias similique rem, est impedit laboriosam amet doloremque commodi aliquam blanditiis! Vel maiores officiis non impedit ipsum quos incidunt consequuntur repellat quaerat?
+                            </div>
+
+                            <v-card-actions class="action-container">
+                                <v-btn depressed variant="outlined" color="primary">
+                                    Angebot einholen
+                                </v-btn>
+                            </v-card-actions>
+                        </v-card>
+                    </v-col>
+
+                    <v-col cols="12" md="4">
+                        <v-card color="transparent" height="80vh"  style="border: 1px solid rgb(var(--v-theme-textcolor))">
+                            <v-card-title class="text-h5 header">
+                                Unlimited music now
+                            </v-card-title>
+
+                            <div class="text-container pa-6" style="height: 75%">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias similique rem, est impedit laboriosam amet doloremque commodi aliquam blanditiis! Vel maiores officiis non impedit ipsum quos incidunt consequuntur repellat quaerat?
+                            </div>
+
+                            <v-card-actions class="action-container">
+                                <v-btn depressed variant="outlined" color="primary">
+                                    Angebot einholen
+                                </v-btn>
+                            </v-card-actions>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-card>
+    </section>
+</template>
+
+<style>
+
+.header {
+    height: 15% !important;
+    width: 100% !important;
+    background-color: rgb(250, 252, 128);
+    color: rgb(var(--v-theme-background));
+    text-align: center !important;
+    font-weight: bold;
+}
+
+.action-container {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center
+}
+</style>
